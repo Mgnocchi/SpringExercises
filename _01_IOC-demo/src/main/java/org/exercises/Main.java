410a9840b016dcb990210f1eaa0ad3f50db06ff5
magno <marco.gnocchi@ppi-italia.it>
2023-02-09 16:48:03 +0100

    01 - Basic implementation with classic java paradigm (no IOC)

--- _01_IOC-demo/src/main/java/org/exercises/Main.java
+++ _01_IOC-demo/src/main/java/org/exercises/Main.java
@@ -3,8 +3,8 @@ package org.exercises;
 public class Main {
     public static void main(String[] args) {
         // get object
-        Coach coach = new BaseballCoach();
+        Coach coach = new TrackCoach();
         // use object
-        System.out.println(coach.getWorkout());
+        System.out.printf("coach says: %s", coach.getWorkout());
     }
 }
---
+++ _01_IOC-demo/src/main/java/org/exercises/TrackCoach.java
@@ -0,0 +1,8 @@
+package org.exercises;
+
+public class TrackCoach implements Coach {
+    @Override
+    public String getWorkout() {
+        return "RUUUUN!!!";
+    }
+}
