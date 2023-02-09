aa3d2e616d3a69cdf52075938ab5d6d23790012d
magno <marco.gnocchi@ppi-italia.it>
2023-02-09 17:20:42 +0100

    01 - Basic IOC implementation. Editing the bean class from the context definition changes the application behaviour: The application is more easily configurable, but to an extent

--- _01_IOC-demo/src/main/java/org/exercises/Main.java
+++ _01_IOC-demo/src/main/java/org/exercises/Main.java
@@ -1,9 +1,16 @@
 package org.exercises;
 
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
+        // Create application context (it makes all the beans, some lazily, some immediately.
+        // Singletons are the default and immediate)
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
         // get object
-        Coach coach = new TrackCoach();
+        Coach coach = context.getBean("coach", Coach.class);
         // use object
         System.out.printf("coach says: %s", coach.getWorkout());
     }
