7591f46f25dec7fccadbd14d0c83d23f3caa0ecf
magno <marco.gnocchi@ppi-italia.it>
2023-02-14 15:41:34 +0100

    MVC 01 - Example of configuration. Be mindful: you need to export a war based on the module as artifact, and it must import spring jars into it for this to run properly on Tomcat

---
+++ B-MVC/_01_mvc-configuration-xml/src/main/java/org/mvc/conf/HomeController.java
@@ -0,0 +1,17 @@
+package org.mvc.conf;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    /* any method returning a string can be used as a Controller method. Any amount of arguments can be used.
+     * here we map the method to the root adress
+     */
+    @RequestMapping("/")
+    public String showHomePage() {
+        // view name to return (with the prefix and suffix specified in the MVC view resolver bean)
+        return "homepage";
+    }
+
+}
