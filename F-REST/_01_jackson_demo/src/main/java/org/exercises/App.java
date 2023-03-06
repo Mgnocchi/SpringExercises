96f0632b9e02685b2997b7ee7214443bae6eab0f
magno <marco.gnocchi@ppi-italia.it>
2023-03-06 11:13:02 +0100

    REST 01: Jackson demo. Jackson is what spring uses to serialize and deserialize jsons coming and being sent via REST calls

--- F-REST/_01_jackson_demo/src/main/java/org/exercises/App.java
+++ F-REST/_01_jackson_demo/src/main/java/org/exercises/App.java
@@ -14,8 +14,30 @@ public class App
     {
         ObjectMapper mapper = new ObjectMapper();
         try {
-            Dude dudo = mapper.readValue(App.class.getClassLoader().getResource("example.json"), Dude.class);
+            Dude dudo = mapper.readValue(
+                    App.class.getClassLoader().getResource("example.json"), Dude.class);
             System.out.println("dude is " + dudo.getName());
+            System.out.println("\n\n");
+
+            ComplexDude drugo = mapper.readValue(
+                    App.class.getClassLoader().getResource("more-complex-example.json"), ComplexDude.class);
+            System.out.println("dude " + drugo.getName() + " from " + drugo.getAddress().getCity() + " knows");
+            if (drugo.getLanguages().length <= 1) {
+                for (int i = 0; i < drugo.getLanguages().length; i++) {
+                    System.out.println(drugo.getLanguages()[i]);
+                }
+            } else {
+                for (int i = 0; i < drugo.getLanguages().length; i++) {
+                    System.out.print(drugo.getLanguages()[i]);
+                    if (i < drugo.getLanguages().length - 2) {
+                        System.out.print(", ");
+                    } else if (i == drugo.getLanguages().length - 2){
+                        System.out.print(" and ");
+                    } else {
+                        System.out.println();
+                    }
+                }
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
---
+++ F-REST/_01_jackson_demo/src/main/java/org/exercises/ComplexDude.java
@@ -0,0 +1,98 @@
+package org.exercises;
+
+public class ComplexDude {
+    String id;
+    String name;
+    String surname;
+    Adress address;
+
+    String[] languages;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Adress getAddress() {
+        return address;
+    }
+
+    public void setAddress(Adress address) {
+        this.address = address;
+    }
+
+    public String[] getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(String[] languages) {
+        this.languages = languages;
+    }
+
+    public class Adress {
+        String street;
+        String city;
+        String stateorregion;
+        String zip;
+        String country;
+
+        public String getStreet() {
+            return street;
+        }
+
+        public void setStreet(String street) {
+            this.street = street;
+        }
+
+        public String getCity() {
+            return city;
+        }
+
+        public void setCity(String city) {
+            this.city = city;
+        }
+
+        public String getStateorregion() {
+            return stateorregion;
+        }
+
+        public void setStateorregion(String stateorregion) {
+            this.stateorregion = stateorregion;
+        }
+
+        public String getZip() {
+            return zip;
+        }
+
+        public void setZip(String zip) {
+            this.zip = zip;
+        }
+
+        public String getCountry() {
+            return country;
+        }
+
+        public void setCountry(String country) {
+            this.country = country;
+        }
+    }
+}
