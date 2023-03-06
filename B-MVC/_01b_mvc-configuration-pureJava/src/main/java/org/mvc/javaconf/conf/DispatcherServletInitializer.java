dfe7e6f9fe49e4f5631738a9778765962dd625df
magno <marco.gnocchi@ppi-italia.it>
2023-03-06 11:54:53 +0100

    MVC 01b: better documentation and added an alternative method

--- B-MVC/_01b_mvc-configuration-pureJava/src/main/java/org/mvc/javaconf/conf/DispatcherServletInitializer.java
+++ B-MVC/_01b_mvc-configuration-pureJava/src/main/java/org/mvc/javaconf/conf/DispatcherServletInitializer.java
@@ -3,6 +3,13 @@ package org.mvc.javaconf.conf;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 // This replaces the web.xml
+
+/**
+ * This class implements WebApplicationInitializer through inheritance and automatically implements a more streamlined
+ * .onStartup method, compared to the example below. I have no idea about pro or cons.
+ *
+ * This means that this class too doesn't need annotations and is bootstrapped by the servlet
+ */
 public class DispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
     @Override
@@ -38,3 +45,23 @@ public class DispatcherServletInitializer extends AbstractAnnotationConfigDispat
         };
     }
 }
+
+// Alternative way to do this:
+/**
+ * Notice how this class is not annotated using Spring annotations: This is bootstrapped by any servlet
+ */
+//public class SpringWebAppInitializer implements WebApplicationInitializer {
+//
+//    @Override
+//    public void onStartup(ServletContext servletContext) throws ServletException {
+//        AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+//        appContext.register(MvcConfig.class);
+//
+//        ServletRegistration.Dynamic dispatcher = servletContext.addServlet(
+//                "SpringDispatcher", new DispatcherServlet(appContext));
+//        dispatcher.setLoadOnStartup(1);
+//        dispatcher.addMapping("/");
+//
+//    }
+//
+//}
