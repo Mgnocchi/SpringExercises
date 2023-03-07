789c2acc822bf98e7a6f248d0447435b73a162ca
magno <marco.gnocchi@ppi-italia.it>
2023-03-07 15:24:58 +0100

    REST 03: Implemented some Exception Handling

--- F-REST/_03_less_dumb_rest/src/main/java/com/rest2/example/application/RestController.java
+++ F-REST/_03_less_dumb_rest/src/main/java/com/rest2/example/application/RestController.java
@@ -1,11 +1,13 @@
 package com.rest2.example.application;
 
+import com.rest2.example.errors.ResourceNotFoundException;
 import com.rest2.example.student.HandledStudent;
 import com.rest2.example.student.Student;
 import com.rest2.example.student.StudentHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -21,17 +23,37 @@ public class RestController {
 
     @GetMapping("/students")
     public List<Student> getSomeStudents() {
+        this.initTestStudents();
+        StudentHandler handler = context.getBean("studentHandler", StudentHandler.class);
+        return handler.getRegisteredCopy();
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        this.initTestStudents();
+        try {
+            StudentHandler handler = context.getBean("studentHandler", StudentHandler.class);
+            return handler.getRegisteredCopy().get(studentId);
+        } catch (IndexOutOfBoundsException e) {
+            throw new ResourceNotFoundException("No Student with index " + studentId + " exists");
+        }
+    }
+
+
+    @Autowired
+    public void setContext(ApplicationContext context) {
+        this.context = context;
+    }
+
+    // SERVICE METHODS -------------------------------------------------------------------------------------------------
+    private void initTestStudents() {
         StudentHandler handler = context.getBean("studentHandler", StudentHandler.class);
         handler.refresh(); //to remove when I add an input system
         HandledStudent current = context.getBean("handledStudent", HandledStudent.class);
         current.set("franco","franchi");
         current = context.getBean("handledStudent", HandledStudent.class);
         current.set("non fa", "ridere");
-        return handler.getRegisteredCopy();
-    }
-
-    @Autowired
-    public void setContext(ApplicationContext context) {
-        this.context = context;
+        current = context.getBean("handledStudent", HandledStudent.class);
+        current.set("Terzo", "Terzi");
     }
 }
